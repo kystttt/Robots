d5f9bdf8ce8ba7f2bbf9cb818e7585c1298a5e01
Sevostianov Leonid <doghoper75@gmail.com>
2025-03-23 17:13:57 +0500

    Revert "Добавил класс WindowSaver, который используется для сохранения и управления параметрами окон"
    This reverts commit d511b6e180eaf09aac1be7aabe1b7bc134b29551.

--- src/main/java/gui/GameWindow.java
+++ src/main/java/gui/GameWindow.java
@@ -1,69 +1,20 @@
 package gui;
 
-import javax.swing.*;
-import java.awt.*;
-import java.util.HashMap;
-import java.util.Map;
+import java.awt.BorderLayout;
 
-import static java.awt.Frame.ICONIFIED;
-import static java.awt.Frame.NORMAL;
+import javax.swing.JInternalFrame;
+import javax.swing.JPanel;
 
-public class GameWindow extends JInternalFrame implements WindowAction {
-
+public class GameWindow extends JInternalFrame
+{
     private final GameVisualizer m_visualizer;
-
-    public GameWindow() {
+    public GameWindow() 
+    {
         super("Игровое поле", true, true, true, true);
         m_visualizer = new GameVisualizer();
-
         JPanel panel = new JPanel(new BorderLayout());
         panel.add(m_visualizer, BorderLayout.CENTER);
         getContentPane().add(panel);
-        setSize(400, 400);
-        setLocation(50, 50);
-        setVisible(true);
         pack();
-    }
-
-    @Override
-    public Map<String, Integer> saveWindowState() {
-        Map<String, Integer> state = new HashMap<>();
-        state.put("x", getLocation().x);
-        state.put("y", getLocation().y);
-        state.put("width", getWidth());
-        state.put("height", getHeight());
-        if (isIcon()) {
-            state.put("state", ICONIFIED);
-        } else {
-            state.put("state", NORMAL);
-        }
-
-        return state;
-    }
-
-    @Override
-    public void loadWindowState(Map<String, Integer> params) {
-        if (params != null) {
-            setLocation(params.getOrDefault("x", 50), params.getOrDefault("y", 50));
-            setSize(params.getOrDefault("width", 400), params.getOrDefault("height", 400));
-            if (params.getOrDefault("state", NORMAL) == ICONIFIED) {
-                try {
-                    setIcon(true);
-                } catch (java.beans.PropertyVetoException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    @Override
-    public String getNameOfWindow() {
-        return "GameWindow";
-    }
-
-    // ===== Методы для работы с игрой =====
-
-    public void updateGame() {
-        m_visualizer.repaint();
     }
 }
